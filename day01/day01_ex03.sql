d06c0f1e1b625a450122e66a9a9954b019d60f53
Maksim <zatannal@student.21-school.ru>
2023-07-27 12:33:58 +0300

    s21-day01/wip -> ex04 : done; started working on ex05

--- day01/day01_ex03.sql
+++ day01/day01_ex03.sql
@@ -1,7 +1,6 @@
 SELECT DISTINCT order_date AS action_date, person_id
 FROM person_order
-WHERE (order_date, person_id) IN (
-    SELECT person_visits.visit_date, person_visits.person_id
-    FROM person_visits
-)
-ORDER BY order_date, person_id DESC;
+INTERSECT
+SELECT visit_date AS action_date, person_id
+FROM person_visits
+ORDER BY action_date, person_id DESC;
