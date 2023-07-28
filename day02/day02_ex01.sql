017db79b9623e3e719683f10184865550dbf9987
Maksim <zatannal@student.21-school.ru>
2023-07-28 15:47:01 +0300

    s21-day02/wip -> ex01 : done; started working on ex02

---
+++ day02/day02_ex01.sql
@@ -0,0 +1,9 @@
+SELECT missing_date::date
+FROM generate_series('2022-01-01'::date, '2022-01-10'::date, '1 day'::interval) AS missing_date
+LEFT JOIN (
+    SELECT visit_date
+    FROM person_visits
+    WHERE person_id = 1 OR person_id = 2
+) AS tmp ON missing_date = visit_date
+WHERE visit_date IS NULL
+ORDER BY visit_date;
