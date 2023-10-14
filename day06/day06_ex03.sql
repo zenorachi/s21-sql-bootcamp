4145b243b384505c6c5b3ace167bfbc1a4170271
Maksim <zatannal@student.21-school.ru>
2023-10-14 15:55:21 +0300

    s21-day06/wip: ex03 done, started working on ex04

---
+++ day06/day06_ex03.sql
@@ -0,0 +1,8 @@
+CREATE UNIQUE INDEX idx_person_discounts_unique
+	ON person_discounts(person_id, pizzeria_id);
+
+SET enable_seqscan=off;
+
+EXPLAIN ANALYZE
+SELECT * FROM person_discounts
+WHERE person_id = 1 AND pizzeria_id = 1;
