e6347df23f04a04e09c2a95e5f12f882f661fac3
Maksim <zatannal@student.21-school.ru>
2023-10-14 15:02:24 +0300

    s21-day06/wip: ex01 done, started working on ex02

---
+++ day06/day06_ex01.sql
@@ -0,0 +1,13 @@
+INSERT INTO person_discounts (id, person_id, pizzeria_id, discount)
+SELECT
+    ROW_NUMBER() OVER() AS id,
+    po.person_id,
+    m.pizzeria_id,
+    CASE
+        WHEN COUNT(*) = 1 THEN 10.5
+        WHEN COUNT(*) = 2 THEN 22
+        ELSE 30
+    END AS discount
+FROM person_order po
+LEFT JOIN menu m ON po.menu_id = m.id
+GROUP BY po.person_id, m.pizzeria_id;
