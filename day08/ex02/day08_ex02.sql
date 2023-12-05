b952242e6e713a0170c5c149b9b4b5a4675bb1d2
21444456 <MMSonkin@sberbank.ru>
2023-12-05 15:31:23 +0300

    s21-day08: ex02 && ex03 -> done; started working with ex04

---
+++ day08/ex02/day08_ex02.sql
@@ -0,0 +1,31 @@
+-- Session 1
+BEGIN;
+SET TRANSACTION ISOLATION LEVEL REPEATABLE READ;
+
+-- Session 2
+BEGIN;
+SET TRANSACTION ISOLATION LEVEL REPEATABLE READ;
+
+-- Session 1
+SELECT * from pizzeria WHERE name = 'Pizza Hut';
+
+-- Session 2
+SELECT * from pizzeria WHERE name = 'Pizza Hut';
+
+-- Session 1
+UPDATE pizzeria SET rating = 4.0 WHERE name = 'Pizza Hut'; 
+
+-- Session 2
+UPDATE pizzeria SET rating = 3.6 WHERE name = 'Pizza Hut'; 
+
+-- Session 1
+COMMIT;
+
+-- Session 2
+COMMIT;
+
+-- Session 1
+SELECT * from pizzeria WHERE name = 'Pizza Hut';
+
+-- Session 2
+SELECT * from pizzeria WHERE name = 'Pizza Hut';
